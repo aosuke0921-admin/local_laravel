17481e0ed261f52a9e3fde484ca6eab60424c42d
aosuke <web@a-code.jp>
2026-07-10 15:19:22 +0900

    update

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -91,13 +91,8 @@
     </div>
 
     <table>
-      <tr>
-        <th>ID</th>
-        <th>社員名</th>
-        <th>ユーザー</th>
-        <th>パスワード</th>
-        <th></th>
-      </tr>
+
+      @include('master_parts.employee_th')
 
       @foreach($member as $val)
 
